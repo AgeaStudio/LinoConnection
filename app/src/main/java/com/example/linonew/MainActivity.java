27ceff05eefee0b22898192b3df2cc19db6b73ef
Dell <ageastudiocs@gmail.com>
2020-05-15 01:29:17 +0700

    isien

--- app/src/main/java/com/example/linonew/MainActivity.java
+++ app/src/main/java/com/example/linonew/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//        iki haq rokok e
+//        suwon
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
