86decbb1edccf07a92447a99e1dcb9f564299bba
AgeaStudio <ageastudiocs@gmail.com>
2020-05-25 01:33:12 +0700

    adyn

---
+++ app/src/main/java/com/example/linonew/distributor/sales/kirim_uang.java
@@ -0,0 +1,16 @@
+package com.example.linonew.distributor.sales;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.linonew.R;
+
+public class kirim_uang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kirim_uang);
+    }
+}
